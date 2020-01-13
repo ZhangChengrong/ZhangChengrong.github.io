2ce36bbc9cebe9de89a9e62b5faa9e034de2500f
ZhangChengrong <2690572824@qq.com>
2020-01-13 23:31:38 +0800

    简历更新

--- blog/src/components/pages/curriculumVitae.vue
+++ blog/src/components/pages/curriculumVitae.vue
@@ -65,6 +65,25 @@
       <div class="info-wrap">
         <h4>
           <svg class="icon" aria-hidden="true">
+            <use xlink:href="#icon-tubiaozhizuomoban3" />
+          </svg> 工作经验总结
+        </h4>
+        <div class="info-body">
+          <div class="info-body-item">
+            <div class="info-content">
+              <p>熟练利用vue相关技术进行管理平台和H5开发</p>
+              <p>1.熟悉vue以及vue有关的实用轮子如vue-router,vuex等，并可手动实现简易版vue-router，vuex</p>
+              <p>2.熟悉vue1.0原理并曾手动实现简易MVVM框架</p>
+              <p>3.阅读过vue2.0源码，知晓相对1.0优势，同时能够简要描述虚拟数与diff算法</p>
+              <p>视频相关</p>
+              <p>工作之余帮助朋友开发过浏览器录屏、音视频播放等功能，对该内容有所了解</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="info-wrap">
+        <h4>
+          <svg class="icon" aria-hidden="true">
             <use xlink:href="#icon-tubiaozhizuomoban4" />
           </svg> 工作经历
         </h4>
@@ -77,21 +96,29 @@
             </div>
             <div class="info-content">
               <p>
-                <strong>2016.06-2016.12 mui应用i厦大</strong> 前端开发，使用技术：mui框架、H5+
+                <strong>涉及开发技术栈</strong> mui框架、H5+、jq+bootstrap+服务端ejs模板引擎、vue全家桶、nodejs(初步了解)
               </p>
               <p>
-                <strong>2017.01-2017.04 辅导员工作平台</strong> 前端开发，使用技术：jq+bootstrap+服务端ejs模板引擎
+                <strong>优秀项目经验</strong> 全国高校学工saas平台
               </p>
               <p>
-                <strong>2017.04-2018.01 全国高校学工平台1.0</strong> 前端开发，使用技术：vuejs+vue-router+ajax
+                <strong>项目推广情况</strong> 目前已经在厦门大学、上海外国语大学、云南财经大学等211/985高校成功应用，作为各个地区的标杆院校，同时辐射周边地区的高校。
               </p>
               <p>
-                <strong>2018.01-2019.02 全国高校学工平台2.0</strong> 前端开发，使用技术：vuejs+vue-router+vuex+ajax
-              </p>
-              <p>
-                <strong>工作总结</strong>：精通vue开发，可以独立开发并维护一些类似element的组件库，如下拉框、表格、树等复杂组件；
-                <br />熟练使用一些前端优化技术和配置对项目进行优化：雪碧图，gzip，路由懒加载，组件按需加载，cdn，缓存等。
-                <br />熟练使用photoshop对asd文件进行切图取色
+                <strong>项目开发情况</strong> 项目围绕两个核心模块进行展开：流程引擎模块和表单引擎模块。
+                <br /><strong>流程引擎：</strong>
+                <br />1.配置表单
+                <br />2.用户手动配置流程节点，确定基本流程环节，包括流转节点(开始、结束、条件、事务)，流转条件，流转分支，节点代理委托
+                <br />3.流程确定之后，针对各个节点进行角色配置，包括操作权限，访问权限
+                <br />4.角色确定之后，只需在角色管理进行账号绑定就可为系统用户提供相对应的流程服务。
+                <br /><strong>表单引擎：</strong>
+                <br />备注：有些事务不需要走审批流，只是一个各个角色参与下的信息收集过程，比如学生信息收集
+                <br />1.配置表单
+                <br />2.用户手动配置新增表单角色(与系统角色为一对多关系，与上述流程节点相似)
+                <br />3.为对应的表单角色配置操作权限与访问权限以及该表单角色下所包含的系统角色
+                <br />4.角色确定之后，只需在角色管理进行账号绑定就可为系统用户提供相对应的表单服务。
+                <br /><strong>二者前提：</strong>
+                <br />表单布局与事件触发器：通过用户手动配置表单展示与事件触发器从而实现在流程和表单引擎操作表单的时候可以实现联动。
               </p>
             </div>
           </div>
@@ -103,19 +130,12 @@
             </div>
             <div class="info-content">
               <p>
-                <strong>对账平台、免疫预约管理平台、便民邮寄管理平台等</strong> 前端开发，使用技术：vuejs+vue-router+vuex+axios/elementUI
+                <strong>涉及开发技术栈</strong> elementUI，MintUI，uniapp
               </p>
               <p>
-                <strong>便民邮寄H5，预约免疫H5</strong> 前端开发， 使用技术： vuejs+vue-router+vuex+axios/MintUI
-              </p>
-              <p>
-                <strong>榕医通计免小程序</strong> 前端开发， 使用技术：uniapp
-              </p>
-              <p>
-                <strong>工作总结：</strong>精通平台类型产品的开发和维护，
-                <br />熟练使用flexiblejs，rem开发H5响应式布局，
-                <br />熟练使用uniapp进行跨平台小程序开发。
-                <br />熟练使用蓝湖。
+                <strong>对账平台、免疫预约管理平台、便民邮寄管理平台等pc端平台</strong>
+                <br /><strong>便民邮寄，预约免疫等公众号H5页面</strong>
+                <br /><strong>榕医通计免小程序</strong>
               </p>
             </div>
           </div>
@@ -125,7 +145,7 @@
         <h4>
           <svg class="icon" aria-hidden="true">
             <use xlink:href="#icon-tubiaozhizuomoban3" />
-          </svg> 技能证书
+          </svg> 技能描述
         </h4>
         <div class="info-body">
           <div class="info-body-item">
@@ -146,7 +166,7 @@
           <div class="info-body-item">
             <div class="info-content">
               <p>个性乐观开朗，积极热情。</p>
-              <p>工作认真负责，勇于挑战，不畏艰难。</p>
+              <p>工作认真负责，吃苦耐劳，勤奋好学，勇于挑战，不畏艰难。</p>
               <p>业余爱好：羽毛球，唱歌，游戏，偶尔逛逛技术博客以及为qq技术群新人提供技术帮助</p>
             </div>
           </div>
